5888dff29ee75db669fe46bf87509ae22fd2313d
surajgit <surajjsingh111@gmail.com>
2020-09-08 00:57:55 +0530

    Updates

---
+++ precache-manifest.dc4fa8c258b9dde283b0ba8840a28483.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51175352cc908e9a3a90a227faf5ef0d",
+    "url": "/Surajjayal/WebChat/index.html"
+  },
+  {
+    "revision": "3c2c094e4d480f98ec8a",
+    "url": "/Surajjayal/WebChat/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "83c5704af90649944b43",
+    "url": "/Surajjayal/WebChat/static/css/main.14bc2e45.chunk.css"
+  },
+  {
+    "revision": "3c2c094e4d480f98ec8a",
+    "url": "/Surajjayal/WebChat/static/js/2.92e6a84f.chunk.js"
+  },
+  {
+    "revision": "7151bfffcb4d0e2aa891867f58c99b0e",
+    "url": "/Surajjayal/WebChat/static/js/2.92e6a84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c5704af90649944b43",
+    "url": "/Surajjayal/WebChat/static/js/main.3fba4428.chunk.js"
+  },
+  {
+    "revision": "577069d12f3af9dd92d2",
+    "url": "/Surajjayal/WebChat/static/js/runtime-main.8d90153f.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/Surajjayal/WebChat/static/media/14-wave_hand.9299706f.png"
+  },
+  {
+    "revision": "b2aedbd285417ea09397174bd107c823",
+    "url": "/Surajjayal/WebChat/static/media/back.b2aedbd2.jpg"
+  },
+  {
+    "revision": "06c56ac7a163625fde104d0d5b2de862",
+    "url": "/Surajjayal/WebChat/static/media/lego1.06c56ac7.png"
+  },
+  {
+    "revision": "71a557405e46fa147fd085c2ff26a86d",
+    "url": "/Surajjayal/WebChat/static/media/lego2.71a55740.png"
+  },
+  {
+    "revision": "06be6044cdfb47188aa4a910a62826bd",
+    "url": "/Surajjayal/WebChat/static/media/lego3.06be6044.png"
+  },
+  {
+    "revision": "1c4ed87570af2eb4fe94f0439481758e",
+    "url": "/Surajjayal/WebChat/static/media/lego4.1c4ed875.png"
+  },
+  {
+    "revision": "141979e5e45b78a696561dd41129b9c6",
+    "url": "/Surajjayal/WebChat/static/media/lego5.141979e5.png"
+  },
+  {
+    "revision": "9aa835e0fe2d7e6ace0fefef6cc563b4",
+    "url": "/Surajjayal/WebChat/static/media/lego6.9aa835e0.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "/Surajjayal/WebChat/static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "/Surajjayal/WebChat/static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "/Surajjayal/WebChat/static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "/Surajjayal/WebChat/static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "/Surajjayal/WebChat/static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "/Surajjayal/WebChat/static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "/Surajjayal/WebChat/static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "/Surajjayal/WebChat/static/media/mimi9.b5ec7c32.gif"
+  },
+  {
+    "revision": "0d019992b4f408dc2af8c9049d2daac0",
+    "url": "/Surajjayal/WebChat/static/media/nopic.0d019992.jpg"
+  },
+  {
+    "revision": "b34a14b0516921c2692895e36652d7c1",
+    "url": "/Surajjayal/WebChat/static/media/suraj_place.b34a14b0.jpg"
+  }
+]);
